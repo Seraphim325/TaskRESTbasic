04cc167d2b7758199c5edc27ba3b056c0f71404f
Seraphim325 <alexey.grishin325@gmail.com>
2026-03-16 12:03:42 +0300

    renamed main class

---
+++ src/main/java/com/example/Application.java
@@ -0,0 +1,14 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class RESTService {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RESTService.class, args);
+	}
+
+}
